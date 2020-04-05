f9c7de482c7a87c240b2a9d6be3dcac0466070e3
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-04-05 19:37:11 +0100

    Implement Auth middleware

---
+++ src/middlewares/auth/index.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { decodeToken } from '../../helpers/jwt'
+import { getRepository } from 'typeorm'
+import { User } from '../../database/entities/User'
+import { respondWithError } from '../../helpers/helpers'
+import { HTTP_UNAUTHORIZED } from '../../helpers/httpStatusCode'
+
+export const isAuthenticated =  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { authorization = ''} = req.headers
+        const { id } = await decodeToken(authorization);
+        if (id) {
+            const user = getRepository(User).find(id)
+            req.user = user;
+            next()
+        } else {
+            respondWithError(res,HTTP_UNAUTHORIZED,'Invalid authorization' )
+        }
+    } catch (error) {
+        respondWithError(res,HTTP_UNAUTHORIZED,'Invalid authorization')
+    }
+}
