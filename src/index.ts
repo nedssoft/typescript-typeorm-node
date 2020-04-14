aa3f8aedfa67cf96f6082f0a43cec17125a44382
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-04-14 16:15:20 +0100

    Fix port bug

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import server from './server';
 import connectionOptions from './database/connectionOptions'
 dotenv.config()
 
-const PORT: any = process.env.HTTP_PORT || 4000;
+const PORT: any = process.env.PORT || 4000;
 
 createConnection(connectionOptions).then(()=> {
     server.listen(PORT,() => { console.log(`Server listening at http://localhost:${PORT}`)});
